4bb7af26c8c65200c164cd5fd713beb0034c0b6f
ardihp <ardihp8@gmail.com>
2022-01-20 02:11:28 +0700

    feat: catch pokemon

--- src/components/Pokeball.js
+++ src/components/Pokeball.js
@@ -1,15 +1,55 @@
 import React from "react";
 import PokeImg from "../assets/pokeball.png";
-import { Link } from "react-router-dom";
 import "../styles/Pokeball.scss";
 
-function Pokeball() {
+function Pokeball({ data }) {
+  const localData = localStorage.getItem("_mypokemon");
+
+  function catchPoke() {
+    const random = Math.floor(Math.random() * 10);
+
+    if (random > 5) {
+      alert("wohoo kamu dapat!");
+
+      const pokeName = prompt("Masukan nama untuk pokemon ini");
+
+      if (localData !== null) {
+        const listPoke = JSON.parse(localData);
+
+        localStorage.setItem(
+          "_mypokemon",
+          JSON.stringify([
+            ...listPoke,
+            {
+              pokemon: data.name,
+              image: data.sprites.front_default,
+              name: pokeName,
+            },
+          ])
+        );
+      } else {
+        localStorage.setItem(
+          "_mypokemon",
+          JSON.stringify([
+            {
+              pokemon: data.name,
+              image: data.sprites.front_default,
+              name: pokeName,
+            },
+          ])
+        );
+      }
+    } else {
+      alert(`Woops, pokemon ${data.name} lepas!`);
+    }
+  }
+
   return (
-    <Link to="/my-pokemon" className="float-poke">
+    <button onClick={() => catchPoke()} type="button" className="float-poke">
       <div className="pokeball">
         <img src={PokeImg} alt="pokeball" />
       </div>
-    </Link>
+    </button>
   );
 }
 
