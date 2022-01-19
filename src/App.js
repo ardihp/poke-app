d1c27ae8ba6c5aa65b73fa2aa96986c6e9b9550c
ardihp <ardihp8@gmail.com>
2022-01-20 00:58:12 +0700

    feat: setup my pokemon path and scroll to top

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "react-query";
 import Header from "./components/Header";
-import Pokeball from "./components/Pokeball";
 import Index from "./pages/Index";
 import "./App.scss";
 import Detail from "./pages/detail";
+import MyPokemon from "./pages/MyPokemon";
+import ScrollTop from "./components/ScrollTop";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="container">
+      <ScrollTop />
       <Header />
       <div className="main">
         <div className="main-content">
@@ -18,11 +20,11 @@ function App() {
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="pokemon/:name" element={<Detail />} />
+              <Route path="my-pokemon" element={<MyPokemon />} />
             </Routes>
           </QueryClientProvider>
         </div>
       </div>
-      <Pokeball />
     </div>
   );
 }
