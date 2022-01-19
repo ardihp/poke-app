52054225bcb2af3f2ad7478f6ec40099ee7bd1dd
ardihp <ardihp8@gmail.com>
2022-01-19 15:46:06 +0700

    feat: setup react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Pokeball from "./components/Pokeball";
+import Index from "./pages/Index";
+import "./App.scss";
+
 function App() {
   return (
-    <div className="text-red-300">
-      <p>hello world</p>
+    <div className="main">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Index />} />
+      </Routes>
+      <Pokeball />
     </div>
   );
 }
