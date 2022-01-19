84c9e60e9725b1f290202d302096b84d7c657dd0
ardihp <ardihp8@gmail.com>
2022-01-19 22:39:51 +0700

    style: fix layout container

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import { Routes, Route } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
 import Header from "./components/Header";
 import Pokeball from "./components/Pokeball";
 import Index from "./pages/Index";
 import "./App.scss";
+import Detail from "./pages/detail";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="main">
+    <div className="container">
       <Header />
-      <Routes>
-        <Route path="/" element={<Index />} />
-      </Routes>
+      <div className="main">
+        <div className="main-content">
+          <QueryClientProvider client={queryClient}>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="pokemon/:name" element={<Detail />} />
+            </Routes>
+          </QueryClientProvider>
+        </div>
+      </div>
       <Pokeball />
     </div>
   );
